174467df2e7e08216446744892237be00edeeb22
Halbout Michael <michaelhalbout@gmail.com>
2017-04-14 10:02:01 +0200

    Update pushNotification.class.php

--- core/class/pushNotification.class.php
+++ core/class/pushNotification.class.php
@@ -22,12 +22,34 @@
 		}
 	}
 	class pushNotificationCmd extends cmd {
+		private function GetToken() {
+			$cache = cache::byKey('pushNotification::token');
+			$token = json_decode($cache->getValue('[]'), true);
+			if($token !== null){     //init the WindowsNotification Class     
+				$Notifier = new WindowsNotification\WindowsNotificationClass();     
+				$Auth = $Notifier->AuthenticateService();     
+				if($Auth->response_status == 200){     
+					cache::set('pushNotification::token', json_encode($Auth), 0);
+				}
+			}
+			return $token;
+		}
 		public function execute($_options = array()) {
 			$uri=$this->getEqlogic()->getConfiguration('adress');
 			switch($this->getEqlogic()->getConfiguration('type_mobile')){
 				//Windows Store and Windows Phone 8.1 (non-Silverlight)
 				case 'windows':
-					
+					$token = $this->GetToken();
+					$Options = new WindowsNotification\WNSNotificationOptions();
+					$Options->SetAuthorization(new WindowsNotification\OAuthObject($token)));
+					$Options->SetX_WNS_REQUESTFORSTATUS(WindowsNotification\X_WNS_RequestForStatus::Request);
+					$Notifier = new WindowsNotification\WindowsNotificationClass($Options);
+					//Send a ToastText02 with custom sounds
+					//$Notifier->Send($uri,WindowsNotification\TemplateToast::ToastText02("HELLO!","I'm the message!!!!",WindowsNotification\TemplateToast::NotificationMail));
+					//Send a ToastText01 to another channel
+					$Notifier->Send($uri,WindowsNotification\TemplateToast::ToastText01($_options['message']));
+					//Send a ToastText01 to another channel with local sound
+					//$Notifier->Send($uri,WindowsNotification\TemplateToast::ToastText01("HELLO!"),WindowsNotification\TemplateToast::CustomSound("<my local url>");
 				break;
 				case 'ios':
 				break;
@@ -36,105 +58,5 @@
 			}
 		}
 	}
-class Notification {
-	public $format;
-	public $payload;
-	# array with keynames for headers
-	# Note: Some headers are mandatory: Windows: X-WNS-Type, WindowsPhone: X-NotificationType
-	# Note: For Apple you can set Expiry with header: ServiceBusNotification-ApnsExpiry in W3C DTF, YYYY-MM-DDThh:mmTZD (for example, 1997-07-16T19:20+01:00).
-	public $headers;
-	function __construct($format, $payload) {
-		if (!in_array($format, ["template", "apple", "windows", "gcm", "windowsphone"])) {
-			throw new Exception('Invalid format: ' . $format);
-		}
-		$this->format = $format;
-		$this->payload = $payload;
-	}
-}
-class NotificationHub {
-	const API_VERSION = "?api-version=2013-10";
-	private $endpoint;
-	private $hubPath;
-	private $sasKeyName;
-	private $sasKeyValue;
-	function __construct($connectionString, $hubPath) {
-		$this->hubPath = $hubPath;
-		$this->parseConnectionString($connectionString);
-	}
-	private function parseConnectionString($connectionString) {
-		$parts = explode(";", $connectionString);
-		if (sizeof($parts) != 3) {
-			throw new Exception("Error parsing connection string: " . $connectionString);
-		}
-		foreach ($parts as $part) {
-			if (strpos($part, "Endpoint") === 0) {
-				$this->endpoint = "https" . substr($part, 11);
-			} else if (strpos($part, "SharedAccessKeyName") === 0) {
-				$this->sasKeyName = substr($part, 20);
-			} else if (strpos($part, "SharedAccessKey") === 0) {
-				$this->sasKeyValue = substr($part, 16);
-			}
-		}
-	}
-	private function generateSasToken($uri) {
-		$targetUri = strtolower(rawurlencode(strtolower($uri)));
-		$expires = time();
-		$expiresInMins = 60;
-		$expires = $expires + $expiresInMins * 60;
-		$toSign = $targetUri . "\n" . $expires;
-		$signature = rawurlencode(base64_encode(hash_hmac('sha256', $toSign, $this->sasKeyValue, TRUE)));
-		$token = "SharedAccessSignature sr=" . $targetUri . "&sig="
-					. $signature . "&se=" . $expires . "&skn=" . $this->sasKeyName;
-		return $token;
-	}
-	public function broadcastNotification($notification) {
-		$this->sendNotification($notification, "");
-	}
-	public function sendNotification($notification, $tagsOrTagExpression) {
-		//echo $tagsOrTagExpression."<p>";
-		if (is_array($tagsOrTagExpression)) {
-			$tagExpression = implode(" || ", $tagsOrTagExpression);
-		} else {
-			$tagExpression = $tagsOrTagExpression;
-		}
-		# build uri
-		$uri = $this->endpoint . $this->hubPath . "/messages" . NotificationHub::API_VERSION;
-		//echo $uri."<p>";
-		$ch = curl_init($uri);
-		if (in_array($notification->format, ["template", "apple", "gcm"])) {
-			$contentType = "application/json";
-		} else {
-			$contentType = "application/xml";
-		}
-		$token = $this->generateSasToken($uri);
-		$headers = [
-		    'Authorization: '.$token,
-		    'Content-Type: '.$contentType,
-		    'ServiceBusNotification-Format: '.$notification->format
-		];
-		if ("" !== $tagExpression) {
-			$headers[] = 'ServiceBusNotification-Tags: '.$tagExpression;
-		}
-		# add headers for other platforms
-		if (is_array($notification->headers)) {
-			$headers = array_merge($headers, $notification->headers);
-		}
-		
-		curl_setopt_array($ch, array(
-		    CURLOPT_POST => TRUE,
-		    CURLOPT_RETURNTRANSFER => TRUE,
-		    CURLOPT_SSL_VERIFYPEER => FALSE,
-		    CURLOPT_HTTPHEADER => $headers,
-		    CURLOPT_POSTFIELDS => $notification->payload
-		));
-		$response = curl_exec($ch);
-		if($response === FALSE){
-		    throw new Exception(curl_error($ch));
-		}
-		$info = curl_getinfo($ch);
-		if ($info['http_code'] <> 201) {
-			throw new Exception('Error sending notificaiton: '. $info['http_code'] . ' msg: ' . $response);
-		}
-	} 
-}
+
 ?>
